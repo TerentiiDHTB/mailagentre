f62c27362cc16e09d3ba8880ec6c7d6e0ce2b31f
YaMamaLul <terentiy51@gmail.com>
2023-12-14 17:47:32 +0500

    todo added

--- src/features/createFolderButton/ui/createFolderModal.tsx
+++ src/features/createFolderButton/ui/createFolderModal.tsx
@@ -3,6 +3,8 @@ import {ModalWindowTemplate} from "@/shared/modalWindowTemplate";
 import {useState} from "react";
 import {createFolder} from "@/features/createFolderButton/model/createFolder.ts";
 
+//TODO: добавить инпута айдишки, добавить лейблам форы. то же самое и для всех остальных.
+
 export const CreateFolderModalWindow = (props: { isShown: boolean, toggle: () => void }) => {
     const [folderName, setFolderName] = useState("")
     const [errMessShow, setErrMessShow] = useState(false)
